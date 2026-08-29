0114117665c152c64d1991e324648cd9f9c17afb
leonardo_rieg <leo.floriano.rieg@gmail.com>
2026-08-28 22:40:19 -0300

    feat: cria interfaces pokemon

---
+++ src/models/pokemon.ts
@@ -0,0 +1,38 @@
+export interface Pokemon {
+    id: number;
+    nome: string;
+    tipos: string[];
+    altura: number;
+    peso: number;
+    hp: number;
+    ataque: number;
+    defesa: number;
+}
+
+export interface PokemonResumo {
+    id: number;
+    nome: string;
+    tipos: string[];
+    altura: number;
+    peso: number;
+}
+
+export interface PokemonApiResponse {
+    id: number;
+    species: {
+        name: string;
+    }
+    height: number;
+    weight: number;
+    stats: {
+        base_stat: number;
+        stat: {
+            name: string;
+        };
+    }[];
+    types: {
+        type: {
+            name: string;
+        };
+    }[];
+}
